6bd4fdefe99b5bbd349573ef50b8ed01b798770c
Do Charity <2793203840@qq.com>
2016-04-24 10:57:08 +0800

    leave some comments

--- api/v1/conn.php
+++ api/v1/conn.php
@@ -1,7 +1,7 @@
 <?php
       @session_start();
-      $dsn = "mysql:host=localhost;dbname=yourDBname";
-      $db = new PDO($dsn, 'user', 'password');
-      $db->query('set names utf8;');
-      date_default_timezone_set('PRC');
+      $dsn = "mysql:host=localhost;dbname=yourDBname";      //设置数据库名称
+      $db = new PDO($dsn, 'user', 'password');              //设置用户名、密码
+      $db->query('set names utf8;');                        //UTF-8格式
+      date_default_timezone_set('PRC');                     //设置时区为UTC+8
 ?>
