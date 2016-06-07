65737c4bb6e200bb10443217d3c26e471730b9c9
John Zhang <zsgsdesign@gmail.com>
2016-06-07 19:46:53 +0800

    Delete conn.php

--- api/v1/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-      @session_start();
-      $dsn = "mysql:host=localhost;dbname=yourDBname";      //设置数据库名称
-      $db = new PDO($dsn, 'user', 'password');              //设置用户名、密码
-      $db->query('set names utf8;');                        //UTF-8格式
-      date_default_timezone_set('PRC');                     //设置时区为UTC+8
-?>
